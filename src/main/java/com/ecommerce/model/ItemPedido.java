8a479ec0d154a6f7da7cc161f5548614395e9c05
Leandro Soares <leoarcabold@gmail.com>
2022-08-15 14:17:11 -0300

    feat: add classe itempedido mapeada

---
+++ src/main/java/com/ecommerce/model/ItemPedido.java
@@ -0,0 +1,24 @@
+package com.ecommerce.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class ItemPedido {
+
+    @EqualsAndHashCode.Include
+    @Id
+    private Long id;
+    private Pedido pedidoId;
+    private Produto produtoId;
+    private BigDecimal precoProduto;
+    private Integer quantidade;
+}
