8f3465d0541a0b81b7116f0aa2653f36d2b9ba94
Leandro Soares <leoarcabold@gmail.com>
2022-08-15 10:48:51 -0300

    feat: add classe cliente mapeada

--- src/main/java/com/ecommerce/model/Cliente.java
+++ src/main/java/com/ecommerce/model/Cliente.java
@@ -1,2 +1,22 @@
-package com.ecommerce.model;public class Cliente {
+package com.ecommerce.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cliente {
+
+    @EqualsAndHashCode.Include
+    @Id
+    private Long id;
+    private String nome;
+    private String sexo;
+
 }
