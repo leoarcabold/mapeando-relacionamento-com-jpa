9e777b2a9c226fdc984a02df7268e9b35dda2f00
Leandro Soares <leoarcabold@gmail.com>
2022-08-17 07:49:56 -0300

    feat: atualização da classe

--- src/main/java/com/ecommerce/util/IniciarUnidadeDePersistencia.java
+++ src/main/java/com/ecommerce/util/IniciarUnidadeDePersistencia.java
@@ -1,16 +1,20 @@
 package com.ecommerce.util;
 
+import com.ecommerce.model.Produto;
+
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
 
 public class IniciarUnidadeDePersistencia {
 
-    public static void main (String[] args){
-        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("Ecommerce-PU");
-
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence
+                .createEntityManagerFactory("Ecommerce-PU");
         EntityManager entityManager = entityManagerFactory.createEntityManager();
 
+        Produto produto = entityManager.find(Produto.class, 1);
+        System.out.println(produto.getNome());
 
         entityManager.close();
         entityManagerFactory.close();
